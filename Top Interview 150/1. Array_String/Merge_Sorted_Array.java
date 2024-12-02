e3c754167167747fe30bdd65d7951b6991ce14d8
Amar Pathak <amarpathak723@gmail.com>
2024-12-02 23:22:25 +0530

    021224 Merge_Sorted_Array

--- Top Interview 150/1. Array_String/Merge_Sorted_Array.java
+++ Top Interview 150/1. Array_String/Merge_Sorted_Array.java
@@ -34,6 +34,63 @@
 
 class Merge_Sorted_Array{
     public static void main(String[] args) {
-        System.out.println("begin");        
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2,5,6};
+        int  n = 3;
+
+        Solution obj = new Solution();
+        obj.merge(nums1, m, nums2, n);
+
+
+
+    }
+}
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+        int i = m - 1; // Pointer for the last valid element in nums1 
+        int j = n - 1; // Pointer for the last element in nums2
+        int k = m + n - 1; // Pointer for the last position in nums1
+
+        // Start merging from the back
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                // Place nums1[i] into the correct position
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+                for(int a: nums1){
+                    System.out.print(a + " ");
+                }
+                System.out.println();
+                for(int a: nums2){
+                    System.out.print(a + " ");
+                }
+                System.out.println();
+            } else {
+                // Place nums2[j] into the correct position
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+                for(int a: nums1){
+                    System.out.print(a + " ");
+                }
+                System.out.println();
+                for(int a: nums2){
+                    System.out.print(a + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        // If there are remaining elements in nums2, copy them
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
+        
     }
 }
